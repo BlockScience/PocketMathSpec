afd4f733ac8f00eaabf6f132d7070f24a01a0bd1
Sean McOwen <seanmcowen@gmail.com>
2023-08-29 14:41:53 -0400

    Update Servicer Params

--- src/StateUpdateTransmissionChannels/Servicer.py
+++ src/StateUpdateTransmissionChannels/Servicer.py
@@ -19,3 +19,23 @@ servicer_transmission_channels.append({"origin": "Prune Servicer QoS",
                                      "entity": "Servicer",
                                      "variable": "Test Scores",
                                      "optional": False})
+
+servicer_transmission_channels.append({"origin": "Update Servicer Params",
+                                     "entity": "Servicer",
+                                     "variable": "Service URL",
+                                     "optional": False})
+
+servicer_transmission_channels.append({"origin": "Update Servicer Params",
+                                     "entity": "Servicer",
+                                     "variable": "Relay Chains",
+                                     "optional": False})
+
+servicer_transmission_channels.append({"origin": "Update Servicer Params",
+                                     "entity": "Servicer",
+                                     "variable": "GeoZone",
+                                     "optional": False})
+
+servicer_transmission_channels.append({"origin": "Update Servicer Params",
+                                     "entity": "Servicer",
+                                     "variable": "Operator Public Key",
+                                     "optional": False})
