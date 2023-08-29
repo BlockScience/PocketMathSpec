e961d5f3d12d4b1599aa5651dc6572c724a727eb
Sean McOwen <seanmcowen@gmail.com>
2023-08-29 10:32:31 -0400

    Add constraints

--- src/Policies/Servicer.py
+++ src/Policies/Servicer.py
@@ -2,7 +2,10 @@ from ..Spaces import servicer_stake_space
 
 servicer_stake_policy = {"name": "Servicer Stake Policy",
                         "description": "",
-                        "constraints": [],
+                        "constraints": ["DOMAIN[0].public_key must not be null",
+                                        "DOMAIN[0].amount > 0",
+                                        "LEN(DOMAIN[0].relay_chains) > 0",
+                                        "All chains in DOMAIN[0].relay_chains must be valid"],
                         "policy_options": [],
                         "domain": [servicer_stake_space],
                         "codomain": [],
