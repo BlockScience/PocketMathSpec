c976c1e75972e3fa9ceffcadbc37783958920d9c
Sean McOwen <seanmcowen@gmail.com>
2023-08-29 11:45:18 -0400

    Add state update

---
+++ src/StateUpdateTransmissionChannels/Servicer.py
@@ -0,0 +1,13 @@
+servicer_transmission_channels = []
+
+servicer_transmission_channels.append({"origin": "Modify Servicer POKT Holdings",
+                                     "entity": "Servicer",
+                                     "variable": "POKT Holdings",
+                                     "optional": False})
+
+servicer_transmission_channels.append({"origin": "Modify Servicer Stake",
+                                     "entity": "Servicer",
+                                     "variable": "Staked POKT",
+                                     "optional": False})
+
+
--- src/StateUpdateTransmissionChannels/__init__.py
+++ src/StateUpdateTransmissionChannels/__init__.py
@@ -1,4 +1,6 @@
 from .Dummy import dummy_transmission_channels
+from .Servicer import servicer_transmission_channels
 
 state_update_transmission_channels = []
 state_update_transmission_channels.extend(dummy_transmission_channels)
+state_update_transmission_channels.extend(servicer_transmission_channels)
