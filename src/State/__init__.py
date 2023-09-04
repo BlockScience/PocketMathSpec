87143ff86cbe2959fd8bd40a5da420658c73c061
Sean McOwen <seanmcowen@gmail.com>
2023-09-04 15:27:10 -0400

    Scaffold application

--- src/State/__init__.py
+++ src/State/__init__.py
@@ -1,7 +1,9 @@
 from .Dummy import dummy_state
 from .Servicer import servicer_state
 from .Fisherman import fisherman_state
+from .Application import application_state
 
 state = {"Dummy": dummy_state,
          "Servicer": servicer_state,
-         "Fisherman": fisherman_state}
+         "Fisherman": fisherman_state,
+         "Application": application_state}
