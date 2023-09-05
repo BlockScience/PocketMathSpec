e7b396aa786303d21ceeb103b2409dfe877cd19f
Sean McOwen <seanmcowen@gmail.com>
2023-09-05 15:25:05 -0400

    Add servicer unpause policy

--- src/Spaces/Servicer.py
+++ src/Spaces/Servicer.py
@@ -1,4 +1,4 @@
-from ..Types import PublicKeyType, uPOKTType, ServiceURLType, RelayChainType, GeoZoneType, ActorType, AddressType
+from ..Types import PublicKeyType, uPOKTType, ServiceURLType, RelayChainType, GeoZoneType, ActorType, AddressType, BlockHeightType
 from typing import TypedDict, List
 
 servicer_stake_space = TypedDict("Servicer Stake Space", {"public_key": PublicKeyType, # The public cryptographic id of the custodial account
@@ -26,6 +26,12 @@ servicer_param_update_space = TypedDict("Servicer Param Update Space", {"public_
 servicer_unpause_space = TypedDict("Servicer Unpause Space", {"actor_type": ActorType,
                                                               "address": AddressType,
                                                               "signer": AddressType,})
+
+# For recording the height of the block at which unpause happens
+servicer_unpause_space2 = TypedDict("Servicer Unpause Space", {"actor_type": ActorType,
+                                                              "address": AddressType,
+                                                              "signer": AddressType,
+                                                              "block_height": BlockHeightType})
 
 #TODO
 servicer_pause_space = TypedDict("Servicer Pause Space", {})
