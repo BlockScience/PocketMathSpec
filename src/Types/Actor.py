4b9414a46a4e946f24e6674fe70de2d6e32f6e97
Sean McOwen <seanmcowen@gmail.com>
2023-09-05 14:53:30 -0400

    Add actor type

---
+++ src/Types/Actor.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class ActorType(Enum):
+    ACTOR_TYPE_UNSPECIFIED = 0
+    ACTOR_TYPE_APP = 1
+    ACTOR_TYPE_SERVICER = 2
+    ACTOR_TYPE_FISH = 3
+    ACTOR_TYPE_VAL = 4
--- src/Types/__init__.py
+++ src/Types/__init__.py
@@ -1,2 +1,3 @@
 from .Dummy import DummyCompoundType, DummyType1, DummyType2
 from .Primitives import PublicKeyType, uPOKTType, ServiceURLType, RelayChainType, GeoZoneType, ServicerReportCardType, ServicerTestScoresType, POKTType, Days
+from .Actor import ActorType
