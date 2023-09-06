8a8fbf2c0b2b91887ff7c8f4e954cc9a837a1282
Sean McOwen <seanmcowen@gmail.com>
2023-09-06 09:42:43 -0400

    Add parameters

---
+++ src/Parameters/Application.py
@@ -0,0 +1,17 @@
+from ..Types import POKTType, Days
+
+application_parameter_set = {"name": "Application Parameter Set",
+                       "notes": "",
+                       "parameters": [{"variable_type": int,
+                                       "name": "minimum_servicers_per_session",
+                                       "description": "The minimum servicers per session",
+                                       "symbol": None,
+                                       "domain": None
+                                       },
+                                       {"variable_type": int,
+                                       "name": "maximum_servicers_per_session",
+                                       "description": "The maximum servicers per session.",
+                                       "symbol": None,
+                                       "domain": None
+                                       },]}
+
