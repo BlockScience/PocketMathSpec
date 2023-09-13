2cadaa0b9cceb9304fa4139a72eba2984261d67d
Sean McOwen <seanmcowen@gmail.com>
2023-09-13 10:48:30 -0400

    Corrections to descriptions

--- src/BoundaryActions/Portal.py
+++ src/BoundaryActions/Portal.py
@@ -2,7 +2,7 @@ from ..Spaces import portal_registration_space
 
 portal_registration = {
     "name": "Portal Registration",
-    "description": "Action for portals to register themselves.",
+    "description": "Registration differs from staking in the sense that the pubKey is known but there are no economic benefits/penalties in this stage of the protocol's progression. The Portal must register on-chain in order for the Servicer to accept its signature as part of the ring. Future versions of the protocol may include on-chain rewards or penalties for the Portal, but the current iteration will incentivize Portals to provide a high quality, highly trusted service through free market economics.",
     "constraints": [],
     "boundary_action_options": [],
     "called_by": ["Portal"],
--- src/Spaces/Portal.py
+++ src/Spaces/Portal.py
@@ -1,7 +1,7 @@
 from typing import TypedDict, List
 from ..Types import PublicKeyType, uPOKTType, ServiceURLType
 
-portal_registration_space = TypedDict("Portal Registration Space", {"public_key": PublicKeyType, # The public cryptographic id of the Application
+portal_registration_space = TypedDict("Portal Registration Space", {"public_key": PublicKeyType, # The public cryptographic id of the Portal account
                                                                 "stake_amount": uPOKTType, # The amount of uPOKT in escrow (i.e. a security deposit)
-                                                          "service_url": ServiceURLType, # The API endpoint where the Web3 service is provided
+                                                          "service_url": ServiceURLType, # The API endpoint where the Portal service is provided
                                                                 })
