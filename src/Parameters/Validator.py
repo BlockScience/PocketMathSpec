f630d35f9ae3bc225da6dc64a80566985e7d9501
Sean McOwen <seanmcowen@gmail.com>
2023-09-15 14:35:24 -0400

    Validator params

---
+++ src/Parameters/Validator.py
@@ -0,0 +1,11 @@
+from ..Types import POKTType
+
+validator_parameter_set = {"name": "Validator Parameter Set",
+                       "notes": "",
+                       "parameters": [{"variable_type": POKTType,
+                                       "name": "minimum_validator_stake",
+                                       "description": "The minimum required stake for validators",
+                                       "symbol": None,
+                                       "domain": None
+                                       },]}
+
