36de2afa813bf9af52fb762606e65abb6c714793
Sean McOwen <seanmcowen@gmail.com>
2023-09-19 17:09:57 -0400

    Add in session data

--- src/Types/Data.py
+++ src/Types/Data.py
@@ -1,3 +1,15 @@
-from typing import NewType, TypedDict
+from typing import NewType, TypedDict, List
+from .Entity import RelayChainEntityType, ApplicationEntityType, ServicerEntityType, FishermanEntityType
+from .Primitives import GeoZoneType
 
-SessionType = NewType('Session', TypedDict('Session', {}))
+SessionType = NewType('Session', TypedDict('Session', {"id": str, # a universally unique ID for the session
+                                                       "session_number": int, # a monotonically increasing number representing the # on the chain
+                                                       "session_height": int, # the height at which the session starts
+                                                       "num_session_blocks": int, # the number of blocks the session is valid from
+                                                       "relay_chain": RelayChainEntityType, # the relay chain the session is valid for
+                                                       "geo_zone": GeoZoneType, # the target geographic region where the actors are present
+                                                       "application": ApplicationEntityType, # the application that is being served
+                                                       "servicers": List[ServicerEntityType], #  the set of servicers that are serving the application
+                                                       "fishermen": List[FishermanEntityType], # the set of fishermen that are fishing for servicers
+                                                       }))
+
--- src/Types/Entity.py
+++ src/Types/Entity.py
@@ -1,3 +1,6 @@
 from typing import NewType
 
 RelayChainEntityType = NewType('Relay Chain Entity', object)
+ApplicationEntityType = NewType('Relay Chain Entity', object)
+ServicerEntityType = NewType('Relay Chain Entity', object)
+FishermanEntityType = NewType('Relay Chain Entity', object)
