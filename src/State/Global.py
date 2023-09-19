b3218f2c3a0d4451489a45afb6df505ff8c1dd4a
Sean McOwen <seanmcowen@gmail.com>
2023-09-19 15:18:27 -0400

    add var

--- src/State/Global.py
+++ src/State/Global.py
@@ -1,4 +1,13 @@
+from typing import List
+from ..Types import RelayChainEntityType
+
 global_state = {"name": "Global State",
               "notes": "",
-              "variables": [
-                             ]}
+              "variables": [{"type": List[RelayChainEntityType],
+                             "name": "Relay Chains",
+                             "description": "The relay chains active in the system",
+                             "symbol": None,
+                             "domain": None}]}
+
+
+
---
+++ src/Types/Entity.py
@@ -0,0 +1,3 @@
+from typing import NewType
+
+RelayChainEntityType = NewType('Relay Chain Entity', object)
