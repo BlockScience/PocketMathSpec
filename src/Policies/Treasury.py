f03a9f52d76bb300241ac85b3cc9cab95edeaec9
Sean McOwen <seanmcowen@gmail.com>
2023-09-25 14:52:11 -0400

    Add transaction fee

--- src/Policies/Treasury.py
+++ src/Policies/Treasury.py
@@ -11,7 +11,7 @@ There are two components that are aggregated here, one for fees and one for gene
                         "codomain": [mint_pokt_mechanism_space, assign_servicer_salary_space, validator_block_reward_space],
                         "parameters_used": ["validator_fee_percentage", "dao_fee_percentage", "relays_to_tokens_multiplier",
                                             "portal_fee_per_relay", "application_fee_per_relay", "block_proposer_allocation",
-                                            "maturity_relay_cost", "maturity_relay_charge", "min_bootstrap_gateway_fee_per_relay",
+                                            "transaction_fee", "maturity_relay_cost", "maturity_relay_charge", "min_bootstrap_gateway_fee_per_relay",
                                             "max_bootstrap_servicer_cost_per_relay", "servicer_bootstrap_unwind_start",
                                             "servicer_bootstrap_end", "gateway_bootstrap_unwind_start", "gateway_bootstrap_unwind_end"]}
 
