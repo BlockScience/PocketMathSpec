1de7b316ec8a65045fc137e90f3c971b3608051f
Sean McOwen <seanmcowen@gmail.com>
2023-10-03 12:24:11 -0400

    Add service state update transmission

---
+++ src/StateUpdateTransmissionChannels/Service.py
@@ -0,0 +1,6 @@
+service_transmission_channels = []
+
+service_transmission_channels.append({"origin": "Add New Service",
+                                     "entity": "Global",
+                                     "variable": "Services",
+                                     "optional": False})
