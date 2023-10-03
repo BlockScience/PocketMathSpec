594092b7b93427430fb76a94474090a805f533d8
Sean McOwen <seanmcowen@gmail.com>
2023-10-03 12:17:32 -0400

    Add service_join_policy_option_v1

--- src/Policies/Service.py
+++ src/Policies/Service.py
@@ -1,9 +1,14 @@
 from ..Spaces import (service_join_space)
 
+service_join_policy_option_v1 = {"name": "Service Join Policy V1",
+                                 "description": "Basic policy option.",
+                                 "logic": "If the servicer id is in the supported_services parameter and it is not already present in the system, continue the action chain. Otherwise terminate."
+                                 }
+
 service_join_policy = {"name": "Service Join Policy",
                         "description": "The policy which decides whether a service can be added to the system.",
                         "constraints": [],
-                        "policy_options": [],
+                        "policy_options": [service_join_policy_option_v1],
                         "domain": [service_join_space],
                         "codomain": [service_join_space],
                         "parameters_used": ["supported_services"]}
