48307b108e75d3b6fc195de882b7d34b69ba478d
Sean McOwen <seanmcowen@gmail.com>
2023-10-30 16:37:11 -0400

    Add geozones to global state

--- src/State/Global.py
+++ src/State/Global.py
@@ -1,28 +1,50 @@
 from typing import List
-from ..Types import ServiceEntityType, SessionType, PortalEntityType, ServicerEntityType
+from ..Types import (
+    ServiceEntityType,
+    SessionType,
+    PortalEntityType,
+    ServicerEntityType,
+    GeoZoneType,
+)
 
-global_state = {"name": "Global State",
-              "notes": "",
-              "variables": [{"type": List[ServiceEntityType],
-                             "name": "Services",
-                             "description": "The services active in the system",
-                             "symbol": None,
-                             "domain": None},
-                             {"type": List[SessionType],
-                             "name": "Sessions",
-                             "description": "A Session is a time-based mechanism used to regulate the various interactions (Web3 access, monitoring, etc) between protocol actors to enable the Utilitarian economy in a fair and secure manner. A single session may extend multiple Blocks as determined by SessionBlockFrequency.",
-                             "symbol": None,
-                             "domain": None},
-                             {"type": List[PortalEntityType],
-                             "name": "Portals",
-                             "description": "Portals held within the system.",
-                             "symbol": None,
-                             "domain": None},
-                             {"type": List[ServicerEntityType],
-                             "name": "Servicers",
-                             "description": "Servicers held within the system.",
-                             "symbol": None,
-                             "domain": None}]}
-
-
-
+global_state = {
+    "name": "Global State",
+    "notes": "",
+    "variables": [
+        {
+            "type": List[ServiceEntityType],
+            "name": "Services",
+            "description": "The services active in the system",
+            "symbol": None,
+            "domain": None,
+        },
+        {
+            "type": List[SessionType],
+            "name": "Sessions",
+            "description": "A Session is a time-based mechanism used to regulate the various interactions (Web3 access, monitoring, etc) between protocol actors to enable the Utilitarian economy in a fair and secure manner. A single session may extend multiple Blocks as determined by SessionBlockFrequency.",
+            "symbol": None,
+            "domain": None,
+        },
+        {
+            "type": List[PortalEntityType],
+            "name": "Portals",
+            "description": "Portals held within the system.",
+            "symbol": None,
+            "domain": None,
+        },
+        {
+            "type": List[ServicerEntityType],
+            "name": "Servicers",
+            "description": "Servicers held within the system.",
+            "symbol": None,
+            "domain": None,
+        },
+        {
+            "type": List[GeoZoneType],
+            "name": "Geozones",
+            "description": "Geozones used within the system.",
+            "symbol": None,
+            "domain": None,
+        },
+    ],
+}
